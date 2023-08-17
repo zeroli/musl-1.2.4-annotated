55e5e5e3e6d152cc012d5f90dd94278ff8947f51
zero.li <zeroliql@gmail.com>
2023-08-17 12:14:42 -0700

    add comments for wait/crt

--- arch/x86_64/crt_arch.h
+++ arch/x86_64/crt_arch.h
@@ -3,10 +3,17 @@ __asm__(
 ".global " START " \n"
 START ": \n"
 "	xor %rbp,%rbp \n"
-"	mov %rsp,%rdi \n"
+"	mov %rsp,%rdi \n" // rdi保存第一个参数，保存着传入参数的个数，整型
 ".weak _DYNAMIC \n"
 ".hidden _DYNAMIC \n"
-"	lea _DYNAMIC(%rip),%rsi \n"
-"	andq $-16,%rsp \n"
-"	call " START "_c \n"
+"	lea _DYNAMIC(%rip),%rsi \n" // rsi保存第二个参数，_DYNAMIC数组的地址，argv数组
+"	andq $-16,%rsp \n"  // align down到16字节边界
+"	call " START "_c \n"  // 调用START_c函数
 );
+
+// 这个头文件被包含在了crt1.c文件中
+// 那里宏定义START = _start
+// 所以call _start_c，就是程序的启动函数
+// _DYNAMIC定义在哪里了？
+// _start_c需要唯一的long*参数p，p[0]就是argc，从p[1]开始就是argv字符串数组
+// 字符串数组就是指针的数组，数组最后一元素为nullptr
--- src/thread/__wait.c
+++ src/thread/__wait.c
@@ -4,11 +4,13 @@ void __wait(volatile int *addr, volatile int *waiters, int val, int priv)
 {
 	int spins=100;
 	if (priv) priv = FUTEX_PRIVATE;
+	// 现在用户空间spin一段时间
 	while (spins-- && (!waiters || !*waiters)) {
 		if (*addr==val) a_spin();
 		else return;
 	}
 	if (waiters) a_inc(waiters);
+	// 然后进入内核空间等待，采用futex系统调用
 	while (*addr==val) {
 		__syscall(SYS_futex, addr, FUTEX_WAIT|priv, val, 0) != -ENOSYS
 		|| __syscall(SYS_futex, addr, FUTEX_WAIT, val, 0);
